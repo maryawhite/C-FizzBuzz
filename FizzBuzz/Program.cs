226ee68857988e5546284b2c56ffab02ea3e9a68
Mary White <mary.a.white28@gmail.com>
2022-02-20 14:45:18 -0500

    basic fizzbuzz method

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,25 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FizzBuzz(50);
+        }
+
+        static void FizzBuzz(int number) {
+            for (int i = 1; i < number; i++) {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("Fizzbuzz");
+                } else if (i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                } else if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                } else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
